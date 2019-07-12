a6e344a4a347a29b797d06026520b5c55ecf12d8
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2019-07-12 23:00:38 +0900

    :neckbeard: rm unuse lib

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import { Editor, EditorBlock, EditorState } from 'draft-js';
 import './lines_number.css';
